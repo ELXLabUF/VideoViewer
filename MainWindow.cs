6d34aac29e562d5937505886ce05a51cc530cf16
StoryLabTAMU <31169870+StoryLabTAMU@users.noreply.github.com>
2019-04-23 18:41:42 -0400

    Add files via upload

---
+++ MainWindow.cs
@@ -0,0 +1,141 @@
+﻿// // Copyright (c) Microsoft. All rights reserved.
+// // Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Diagnostics;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Forms;
+
+namespace VideoViewerDemo
+{
+    /// <summary>
+    ///     Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private bool autoPlay = false;
+        private MyVideos videos;
+        public MainWindow()
+        {
+            InitializeComponent();
+            videos = this.Resources["Vids"] as MyVideos;
+            //videos.Directory = Environment.CurrentDirectory + "\\media";
+        }
+        
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            autoPlay = true;
+        }
+
+        private void MainVideo_MediaEnded(object sender, RoutedEventArgs e)
+        {
+            if (!autoPlay)
+            {
+                return;
+            }
+            MediaElement element = (MediaElement)sender;
+            // Find next video
+            int index = -1;
+            for (int i = 0; i < videos.Count; ++i)
+            {
+                if (videos[i].Uri == element.Source)
+                {
+                    index = i;
+                    break;
+                }
+            }
+            index += 1;
+            // Play it if found
+            if (index >= 0 && index < videos.Count)
+            {
+                element.Source = videos[index].Uri;
+            }
+            // Stop auto-playing if not found
+            else
+            {
+                autoPlay = false;
+            }
+        }
+
+        //v0.1
+
+        private void OnVideosDirChangeClick(object sender, RoutedEventArgs e)
+        {
+            videos.Directory = VideosDir.Text;
+        }
+
+        private void OnLoaded(object sender, RoutedEventArgs e)
+        {
+            VideosDir.Text = Environment.CurrentDirectory + "\\media";
+        }
+
+        private void VideosDir_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Browse(object sender, RoutedEventArgs e)
+        {
+            using (var fbd = new FolderBrowserDialog())
+            {
+                DialogResult result = fbd.ShowDialog();
+
+                if (result == System.Windows.Forms.DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+                {
+                    VideosDir.Text = fbd.SelectedPath;
+                    videos.Directory = VideosDir.Text;
+                }
+            }
+        }
+        //end
+
+
+
+    }
+}
+
+//self-added, all of below
+
+public static class Extensions
+{
+    public static string UpperFolder(this string pFolderName, Int32 pLevel)
+    {
+        List<string> TheList = new List<string>();
+
+        while (!string.IsNullOrEmpty(pFolderName))
+        {
+            var temp = Directory.GetParent(pFolderName);
+            if (temp == null)
+            {
+                break;
+            }
+
+            pFolderName = Directory.GetParent(pFolderName).FullName;
+            TheList.Add(pFolderName);
+
+        }
+
+        if (TheList.Count > 0 && pLevel > 0)
+        {
+            if (pLevel - 1 <= TheList.Count - 1)
+            {
+                return TheList[pLevel - 1];
+            }
+            else
+            {
+                return pFolderName;
+            }
+        }
+        else
+        {
+            return pFolderName;
+        }
+    }
+    public static string SolutionFolder(this string pSender)
+    {
+        return pSender.UpperFolder(4);
+    }
+}
--- README.md
+++ README.md
@@ -1,13 +1,12 @@
-# VideoViewer
-Notes for using:
-Path change has 2 button, "browse" and "change".
-Browse means it will pop up a windows file explorer dialog and you will choose the path using GUI,
-Change means you need to type in/copy the path from the file explorer by yourself, and then click change button to apply the change, the video list will automaticly update.
+# Video Viewer Demo
+This sample creates a video viewing application that demonstrates styling, data binding, and data templating features.
 
-Play all is the button in the bottom.
-If you click it, it will play non-stop from the video that is currently playing to the last video.
-You have the freedom to choose which video to start with (before you click "play all".
-However, once it starts, there's no way to change video playing as it's queued task. I can modify this but I think it's not necessary for current situation as you just need to close the program and restart again so it will be reset.
+## Build the sample
+The easiest way to use these samples without using Git is to download the zip file containing the current version (using the link below or by clicking the "Download ZIP" button on the repo page). You can then unzip the entire archive and use the samples in [Visual Studio 2015](https://www.visualstudio.com/wpf-vs).
 
-Also, although you have the freedom to change the path once the program runs, please put the default videos in the path "C:\testingvideos" for the program to initialize.
+### Deploying the sample
+- Select Build > Deploy Solution. 
+
+### Deploying and running the sample
+- To debug the sample and then run it, press F5 or select Debug >  Start Debugging. To run the sample without debugging, press Ctrl+F5 or selectDebug > Start Without Debugging. 
 
